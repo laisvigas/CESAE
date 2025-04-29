c37291ebe8935f8095165ad284f9d424a9b1c363
laisvigas <laisvigas@outlook.com>
2025-04-29 10:11:08 +0100

    ex02

---
+++ Algorithms/FichasPraticas/src/FichaPratica03/Ex02.java
@@ -0,0 +1,15 @@
+package FichaPratica03;
+
+// Faça um programa que imprima os números pares no intervalo de 1 a 400 inclusive.
+// (Não necessita de Scanner ou input do utilizador).
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int numero = 0;
+
+        while(numero <= 250) {
+            System.out.println(numero);
+            numero += 2;
+        }
+    }
+}
