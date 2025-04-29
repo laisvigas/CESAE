7ab48f450a1dff74fa134c07863ba45a3121186e
laisvigas <laisvigas@outlook.com>
2025-04-29 10:08:09 +0100

    adds ex 17 to do

---
+++ Algorithms/FichasPraticas/src/FichaPratica02/Ex17.java
@@ -0,0 +1,19 @@
+package FichaPratica02;
+import java.util.Scanner;
+
+// Um banco autoriza a conceção de um crédito especial aos seus clientes, num montante variável com base no seu
+//saldo médio do último ano. Faça um programa que leia o saldo médio de um cliente e calcule o valor do crédito
+//especial de acordo com a seguinte tabela. Mostre uma mensagem com o saldo médio e o valor de crédito.
+
+// Saldo Médio ------------------- Percentagem
+//De 0 a 2000 (inclusive) -------- Nenhum crédito
+//De 2000 a 4000 (inclusive) ----- 20% do valor do saldo médio
+//De 4000 a 6000 (inclusive) ----- 30% do valor do saldo médio
+//Acima de 6000 -------------------40% do valor do saldo médio
+
+
+public class Ex17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
