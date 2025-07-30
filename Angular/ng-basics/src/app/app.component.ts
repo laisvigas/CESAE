af575041ae3737c2e08430f804d45fa2a8cf575b
Laís Vigas <laisvigas@outlook.com>
2025-07-30 19:55:35 +0100

    more angular things

--- Angular/ng-basics/src/app/app.component.ts
+++ Angular/ng-basics/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {first} from 'rxjs';
 import {CardComponent} from './card/card.component';
 import {CardsComponent} from './cards/cards.component';
+import {HeaderComponent} from './master-page/header/header.component';
+import {FooterComponent} from './master-page/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardComponent, CardsComponent],
+  imports: [RouterOutlet, CardComponent, CardsComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Angular/ng-basics/src/app/master-page/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MdbCollapseModule} from 'mdb-angular-ui-kit/collapse';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MdbCollapseModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
